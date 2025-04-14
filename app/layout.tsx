00829b612344cab0ae3b16c0f000356c404c0837
gwatsin <gwatsinthong@gmail.com>
2025-04-14 20:29:33 +0530

    Went back to old versions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,18 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import Navbar from "@/components/navigation/navbar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
   variable: "--font-inter",
   weight: "100 200 300 400 500 600 700 800 900",
+});
+
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 700",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${inter.variable} antialiased`}
+        className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
@@ -34,6 +41,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
           >
+            <Navbar/>
              {children}
         </ThemeProvider>
        
