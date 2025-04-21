a5108bcabbc024089890dcde370a8f691b0c4a75
gwatsin <gwatsinthong@gmail.com>
2025-04-21 22:54:22 +0530

    added right sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import {ReactNode} from 'react';
 import Navbar from '@/components/navigation/navbar'
 import LeftSidebar from '@/components/navigation/LeftSidebar';
+import RightSidebar from '@/components/navigation/RightSidebar';
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (<main className="background-light850_dark100 relative">
@@ -8,13 +9,13 @@ const RootLayout = ({children}: {children: ReactNode}) => {
     <Navbar />
     <div className="flex">
       <LeftSidebar />
-    </div>
     <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
       <div className="mx-auto w-full max-w-5xl">
           {children}
       </div>
     </section>
-
+      <RightSidebar />
+    </div>
   </main>)
 }
 
