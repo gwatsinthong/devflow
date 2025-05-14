06134e13a21ce9f2ddfdef2615991ba768063887
gwatsin <gwatsinthong@gmail.com>
2025-05-14 22:13:25 +0530

    implement tag input

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -1,78 +1,128 @@
-'use client'
-// InitializedMDXEditor.tsx
+"use client";
+
 import {
-    headingsPlugin,
-    listsPlugin,
-    quotePlugin,
-    thematicBreakPlugin,
-    markdownShortcutPlugin,
-    MDXEditor,
-    type MDXEditorMethods,
-    toolbarPlugin,
-    ConditionalContents,
-    toolbarContents$,
-    ChangeCodeMirrorLanguage,
-    UndoRedo,
-} from '@mdxeditor/editor'
-import type { ForwardedRef } from 'react';
-import { basicDark } from 'cm6-theme-basic-dark'
-import './dark-editor.css';
-import { useTheme } from 'next-themes';
-import { Separator } from '@radix-ui/react-dropdown-menu';
+  MDXEditor,
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  toolbarPlugin,
+  CodeToggle,
+  InsertCodeBlock,
+  codeBlockPlugin,
+  headingsPlugin,
+  listsPlugin,
+  linkPlugin,
+  quotePlugin,
+  markdownShortcutPlugin,
+  ListsToggle,
+  linkDialogPlugin,
+  CreateLink,
+  InsertImage,
+  InsertTable,
+  tablePlugin,
+  imagePlugin,
+  codeMirrorPlugin,
+  ConditionalContents,
+  ChangeCodeMirrorLanguage,
+  Separator,
+  InsertThematicBreak,
+  diffSourcePlugin,
+  MDXEditorMethods,
+} from "@mdxeditor/editor";
+import { basicDark } from "cm6-theme-basic-dark";
+import { useTheme } from "next-themes";
+import { Ref } from "react";
+
+import "@mdxeditor/editor/style.css";
+import "./dark-editor.css";
 
 interface Props {
-        value: string; 
-        fieldChange: (value: string) => void; 
-        editorRef: ForwardedRef<MDXEditorMethods> | null; 
+  value: string;
+  editorRef: Ref<MDXEditorMethods> | null;
+  fieldChange: (value: string) => void;
 }
 
-const Editor = ({
-    value,
-    editorRef,
-    fieldChange,
-    ...props
-  }: Props) => {
-    const { resolvedTheme } = useTheme();
+const Editor = ({ value, editorRef, fieldChange }: Props) => {
+  const { resolvedTheme } = useTheme();
 
-    const theme = resolvedTheme === 'dark' ? [basicDark] : [];
+  const themeExtension = resolvedTheme === "dark" ? [basicDark] : [];
 
-    return (
-      <MDXEditor
-        key={resolvedTheme}
-        markdown={value}
-        ref={editorRef}
-        className="background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border"
-        onChange={fieldChange}
-        plugins={[
-          headingsPlugin(),
-          listsPlugin(),
-          quotePlugin(),
-          thematicBreakPlugin(),
-          markdownShortcutPlugin(),
-          toolbarPlugin({
-            toolbarContents: () => {
+  return (
+    <MDXEditor
+      key={resolvedTheme}
+      markdown={value}
+      ref={editorRef}
+      onChange={fieldChange}
+      className="background-light800_dark200 light-border-2 markdown-editor dark-editor grid w-full border"
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
+        quotePlugin(),
+        markdownShortcutPlugin(),
+        tablePlugin(),
+        imagePlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            css: "css",
+            txt: "txt",
+            sql: "sql",
+            html: "html",
+            sass: "sass",
+            scss: "scss",
+            bash: "bash",
+            json: "json",
+            js: "javascript",
+            ts: "typescript",
+            "": "unspecified",
+            tsx: "TypeScript (React)",
+            jsx: "JavaScript (React)",
+          },
+          autoLoadLanguageSupport: true,
+          codeMirrorExtensions: themeExtension,
+        }),
+        diffSourcePlugin({ viewMode: "rich-text", diffMarkdown: "" }),
+        toolbarPlugin({
+          toolbarContents: () => (
             <ConditionalContents
               options={[
                 {
-                  when: (editor) => editor?.editorType === 'codeblock',
-                  contents: () => <ChangeCodeMirrorLanguage />
+                  when: (editor) => editor?.editorType === "codeblock",
+                  contents: () => <ChangeCodeMirrorLanguage />,
                 },
                 {
                   fallback: () => (
                     <>
                       <UndoRedo />
                       <Separator />
+
+                      <BoldItalicUnderlineToggles />
+                      <CodeToggle />
+                      <Separator />
+
+                      <ListsToggle />
+                      <Separator />
+
+                      <CreateLink />
+                      <InsertImage />
+                      <Separator />
+
+                      <InsertTable />
+                      <InsertThematicBreak />
+                      <Separator />
+
+                      <InsertCodeBlock />
                     </>
-                  )
-                }
+                  ),
+                },
               ]}
             />
-            }
-          })
-        ]}
-        {...props}
-      />
-    );
-  };
+          ),
+        }),
+      ]}
+    />
+  );
+};
 
-export default Editor
+export default Editor;
--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,120 +1,193 @@
-"use client"
+"use client";
 
-import { AskQuestionSchema } from '@/lib/validations'
-import { zodResolver } from '@hookform/resolvers/zod'
-import React, { useRef } from 'react'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import dynamic from "next/dynamic";
+import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
-import { Input } from '../ui/input'
-import { Button } from '../ui/button'
-import dynamic from 'next/dynamic'
-import { MDXEditorMethods } from '@mdxeditor/editor';
+import { z } from "zod";
+
+import { AskQuestionSchema } from "@/lib/validations";
+
+import TagCard from "../cards/TagCard";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
 
 const Editor = dynamic(() => import("@/components/editor"), {
-    ssr: false
-} )
+  ssr: false,
+});
 
 const QuestionForm = () => {
+  // 创建一个ref，用于存储MDXEditorMethods
   const editorRef = useRef<MDXEditorMethods>(null);
 
-    const form = useForm({
-        resolver: zodResolver(AskQuestionSchema),
-        defaultValues: {
-            title: "",
-            content: "",
-            tags: []
-        }
-    })
-    const handleCreateQuestion = () => {};
+  // 使用useForm钩子，创建一个表单，并使用zodResolver验证表单数据
+  const form = useForm<z.infer<typeof AskQuestionSchema>>({
+    resolver: zodResolver(AskQuestionSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      tags: [],
+    },
+  });
 
-  return <Form {...form}>
-    <form className="flex w-full flex-col gap-10" onSubmit={form.handleSubmit(handleCreateQuestion)
-    }>
-    
-    <FormField
-            control={form.control}
-            name="title"
-            render={({ field }) => (
-              <FormItem className="flex w-full flex-col">
-                <FormLabel className="paragraph-semibold text-dark400_light800">
-                    Question Title <span className="text-primary-500">*</span>
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
-                  />
-                </FormControl>
-                <FormDescription className="bodu-regular text-light-500 mt-2.5">
-                  Be specific and imagine you&apos;re asking a question to another person.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: { value: string[] }
+  ) => {
+    console.log(field, e);
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const tagInput = e.currentTarget.value.trim();
 
+      if (tagInput && tagInput.length < 15 && !field.value.includes(tagInput)) {
+        form.setValue("tags", [...field.value, tagInput]);
+        e.currentTarget.value = "";
+        form.clearErrors("tags");
+      } else if (tagInput.length > 15) {
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag should be less than 15 characters",
+        });
+      } else if (field.value.includes(tagInput)) {
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag already exists",
+        });
+      }
+    }
+  };
 
-<FormField
-            control={form.control}
-            name="content"
-            render={({ field }) => (
-              <FormItem className="flex w-full flex-col">
-                <FormLabel className="paragraph-semibold text-dark400_light800">
-                    Detailed explaination of your problem<span className="text-primary-500">*</span>
-                </FormLabel>
-                <FormControl>
+  const handleTagRemove = (tag: string, field: { value: string[] }) => {
+    const newTags = field.value.filter((t) => t !== tag);
 
-                  <Editor 
-                    value={field.value} 
-                    editorRef={editorRef} 
-                    fieldChange={field.onChange} 
-                  />
+    form.setValue("tags", newTags);
 
-                </FormControl>
-                <FormDescription className="bodu-regular text-light-500 mt-2.5">
-                  Introduce the problem and explain why it matters to you. Keep it short and simple.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+    if (newTags.length === 0) {
+      form.setError("tags", {
+        type: "manual",
+        message: "Tags are required",
+      });
+    }
+  };
 
+  const handleCreateQuestion = (data: z.infer<typeof AskQuestionSchema>) => {
+    console.log(data);
+  };
 
-<FormField
-            control={form.control}
-            name="tags"
-            render={({ field }) => (
-              <FormItem className="flex w-full flex-col gap-3">
-                <FormLabel className="paragraph-semibold text-dark400_light800">
-                    Tags <span className="text-primary-500">*</span>
-                </FormLabel>
-                <FormControl>
-                    <div>
+  return (
+    <Form {...form}>
+      <form
+        className="flex w-full flex-col gap-10"
+        onSubmit={form.handleSubmit(handleCreateQuestion)}
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Question Title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Be specific and imagine you&apos;re asking a question to another
+                person.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Detailed explanation of your problem{" "}
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Editor
+                  value={field.value}
+                  editorRef={editorRef}
+                  fieldChange={field.onChange}
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Introduce the problem and expand on what you&apos;ve put in the
+                title.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Tags <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <div>
                   <Input
                     className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
                     placeholder="Add tags..."
-                    {...field}
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
                   />
-                  Tags
-
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 flex-wrap gap-2.5">
+                      {field?.value?.map((tag: string) => (
+                        <TagCard
+                          key={tag}
+                          _id={tag}
+                          name={tag}
+                          compact
+                          remove
+                          isButton
+                          handleRemove={() => handleTagRemove(tag, field)}
+                        />
+                      ))}
                     </div>
-                </FormControl>
-                <FormDescription className="bodu-regular text-light-500 mt-2.5">
-                  Add upto 3 tags to describe what your question is about. You need to press enter to add a tag.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        <div className="mt-6 flex justify-end">
-            <Button type="submit" className="primary-gradient w-fit !text-light-900">
+                  )}
+                </div>
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Add up to 3 tags to describe what your question is about. You
+                need to press enter to add a tag.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="mt-16 flex justify-end">
+          <Button
+            type="submit"
+            className="primary-gradient w-fit !text-light-900"
+          >
             Ask A Question
-            </Button>
+          </Button>
         </div>
-    </form>
-  </Form>
-}
+      </form>
+    </Form>
+  );
+};
 
-
-
-export default QuestionForm
+export default QuestionForm;
